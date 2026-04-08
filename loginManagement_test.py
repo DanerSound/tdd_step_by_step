04778350bd9463f6663c904f628c8558ca620aa0
DanerSound <andre.barreto28@gmail.com>
2026-04-08 16:35:17 +0200

    added ex 22 base configuration

---
+++ loginManagement_test.py
@@ -0,0 +1,6 @@
+"""
+Scrivi un programma che crei un file CSV per memorizzare in un dizionario i dati degli utenti registrati su un sito web.
+I dati richiesti per ogni utente sono: username, password, email e data di registrazione. 
+Il programma deve permettere di salvare le informazioni nel file, leggere i dati e stamparli a schermo.
+
+"""
